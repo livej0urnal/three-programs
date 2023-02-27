0a4901dcee97f53d7b2c9e335b4977f2421b2b73
Igor Filippov <anubis3009@gmail.com>
2023-02-27 17:51:34 +0200

    #2-program

--- first.py
+++ first.py
@@ -1,5 +1,8 @@
 import moviepy.editor
+from pathlib import Path
 
-video = moviepy.editor.VideoFileClip('my_video.mp4')
+video_file = Path('my_video.mp4')
+
+video = moviepy.editor.VideoFileClip(f'{video_file}')
 audio = video.audio
-audio.write_audiofile('my_audio.mp3')
+audio.write_audiofile(f'{video_file.stem}.mp3')
