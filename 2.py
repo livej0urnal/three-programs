bdfe877f58339504923fb4dca8f88d7c95161b13
Igor Filippov <anubis3009@gmail.com>
2023-02-27 17:55:38 +0200

    #2-program

--- 2.py
+++ 2.py
@@ -1,5 +1,9 @@
 import pyAesCrypt
 
 password = input('Enter password to crypto: ')
-#encrypt
+# encrypt
 pyAesCrypt.encryptFile('data.txt', 'data.txt.aes', password)
+
+# decrypt
+pyAesCrypt.decryptFile('data.txt.aes', 'dataout.txt', password)
+1
